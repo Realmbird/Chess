0eb0a2f1faf7405c52063d88dde40e737b083369
Realmbird <happychristopher777@gmail.com>
2023-03-02 19:25:04 -0800

    Added Unit Tests to Chess Added Board Class Added Color Class

--- lib/Chess.rb
+++ lib/Chess.rb
@@ -1,3 +1,29 @@
-class Chess 
+require_relative './Board'
+# plays Chess
+class Chess
+  def initialize(player_1 = 'player_1', player_2 = 'player_2')
+    @player_1 = player_1
+    @player_2 = player_2
+    @Chess_Board = Board.new
+  end
 
+  # gets required info
+  def game_setup
+    puts "Enter name for player 1"
+    @player_1 = gets.chomp
+    puts "Enter name for player 2"
+    @player_2 = gets.chomp
+  end
+
+  #The actual play code
+  def run_game
+    play while !@Chess_Board.win
+    @Chess_Board.reset
+  end
+
+  # gets required info and starts game
+  def game_start
+    game_setup
+    run_game
+  end
 end
--- spec/Chess_spec.rb
+++ spec/Chess_spec.rb
@@ -1,5 +1,20 @@
-require_relative '../lib/Chess.rb'
+require_relative '../lib/Chess'
 
 describe Chess do
-
+  describe '#Game_Setup' do
+    subject(:intro_game){described_class.new() }
+    before do
+      allow(intro_game).to receive(:puts).exactly(2).time
+      allow(intro_game).to receive(:gets).and_return('Jhammer')
+      
+    end
+    it 'prompts the user for a players names' do
+      expect(intro_game).to receive(:gets).exactly(2).time
+      
+      intro_game.game_setup
+    end
+  end
+  describe "#run_game" do
+    subject(:demo_game){described_class.new()}
+  end
 end
