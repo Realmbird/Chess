6743e68669b13bc8864bfd7b3a2bd762709a088b
Realmbird <happychristopher777@gmail.com>
2023-03-05 18:54:58 -0800

    fixed print

--- lib/Board.rb
+++ lib/Board.rb
@@ -4,19 +4,27 @@ class Board
   include ColorableString
   include Assets
   attr_accessor :Board, :Win
-  def initialize ()
-    @Board = Array.new(8) { Array.new(8, "a")}
+  def initialize
+    @Board = [
+      [black_rook, black_knight, black_bishop, black_queen, black_king, black_bishop, black_knight,
+       black_rook], [black_pawn, black_pawn, black_pawn, black_pawn, black_pawn, black_pawn, black_pawn, black_pawn], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn], [white_rook, white_knight, white_bishop, white_queen, white_king, white_bishop, white_knight, white_rook]
+    ]
+    # originally tried @Board Array.new(8){Array.new(8, "")} did not work
     @Win = false
   end
-  def printBoard 
-    p %w[ ,a, b, c, d, e, f, g, h]
+
+  def printBoard
+    puts '  abcdefgh '
     @Board.each_with_index do |row, index|
-      p "#{index} #{row.each_with_index{
-        |element,e_index|
+      colored_row = row.map.with_index do |element, e_index|
         color_index = index + e_index
-        element.bg_color(:white) if(color_index % 2 == 0)
-        element.bg_color(:black) if(color_index % 2 == 1)
-      }} #{index}"
+        if color_index.even?
+          element.to_s.ljust(5).bg_color(:green)
+        else
+          element.to_s.ljust(5).bg_color(:cyan)
+        end
+      end
+      puts "#{8 - index} #{colored_row.join} #{8 - index}"
     end
   end
   def converter(input)
