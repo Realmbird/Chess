87ae6f338b96c0d973552a978a36afde18a89a71
Realmbird <happychristopher777@gmail.com>
2023-03-05 18:45:52 -0800

    Added isValid Have bugs with letter converter

--- lib/Board.rb
+++ lib/Board.rb
@@ -1,11 +1,11 @@
 require_relative './Details/Color'
 require_relative './Details/Assests'
 class Board 
-  using ColorableString
+  include ColorableString
   include Assets
   attr_accessor :Board, :Win
   def initialize ()
-    @Board = [['','','','','','','',''],['','','','','','','','']]
+    @Board = Array.new(8) { Array.new(8, "a")}
     @Win = false
   end
   def printBoard 
@@ -14,11 +14,29 @@ class Board
       p "#{index} #{row.each_with_index{
         |element,e_index|
         color_index = index + e_index
-        element.bg_color(:white) if(color_index %2 == 0)
-        element.bg_color(:black) if(color_index %2 == 1)
+        element.bg_color(:white) if(color_index % 2 == 0)
+        element.bg_color(:black) if(color_index % 2 == 1)
       }} #{index}"
     end
   end
+  def converter(input)
+    letter = letter_index(input[0])
+    number = input[1]
+    chess_row = 8 - number.to_i
+    chess_col = letter_index(letter)
+    [chess_row, chess_col]
+  end
+  def letter_index(letter)
+    letter.to_s.downcase.ord - "a".ord
+  end
+  def isValid(row, col)
+    if @Board[row].nil? || @Board[row][col].nil?
+      false
+    else
+      true
+    end
+  end
+  def isAdded()
+
+  end
 end
-board = Board.new
-board.printBoard
---
+++ spec/Board_spec.rb
@@ -0,0 +1,28 @@
+require_relative '../lib/Board'
+describe Board do
+  
+  describe "#isValid" do
+    subject(:validator){ described_class.new()}
+    it 'returns false when column is false' do
+      expect(validator.isValid(8,8)).to eq(false)
+    end
+    it 'return false when row is false' do
+      expect(validator.isValid(0,8)).to eq(false)
+    end
+    it 'returns true when row and column is valid' do
+      expect(validator.isValid(0,0)).to eq(true)
+    end
+  end
+  describe 'letter index' do
+    subject(:index_finder){described_class.new}
+    it 'returns 0' do
+      expect(index_finder.letter_index('a')).to eq(0)
+    end
+  end
+  describe 'letter converter' do
+    subject(:letter_converter) {described_class.new}
+    it 'returns [0,0]' do
+      expect(letter_converter.converter('a8')).to eq([0,0])
+    end
+  end
+end
